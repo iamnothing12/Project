26a54a83d9ab07600aec4090312ebaee136e58d7
iamnothing12 <derwin_lim@hotmail.com>
2019-11-05 23:03:53 +0800

    Update URL PatternMatch

--- beautifulworld.py
+++ beautifulworld.py
@@ -1,19 +1,36 @@
 from bs4 import BeautifulSoup
 import urllib3
+import re
 
-http = urllib3.PoolManager()
+# http = urllib3.PoolManager()
 
-url = "https://www.pythonforbeginners.com"
-content = http.request('GET',url,headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'})
-soup = BeautifulSoup(content.data, features='lxml')
+# url = "https://www.pythonforbeginners.com"
+# content = http.request('GET',url,headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'})
+# soup = BeautifulSoup(content.data, features='lxml')
 
-# print(soup.prettify)
+# # print(soup.prettify)
 
-for link in soup.find_all('link'):
-    print(link.get('href'))
+# for link in soup.find_all('link'):
+#     print(link.get('href'))
 
-for link in soup.find_all('a'):
-    print(link.get('href'))
+# for link in soup.find_all('a'):
+#     print(link.get('href'))
 
-for link in soup.find_all('img'):
-    print(link.get('src'))
+# for link in soup.find_all('img'):
+#     print(link.get('src'))
+
+urlPattern = re.compile(r'^((http:\/\/|https:\/\/)(([a-zA-Z0-9\-\%])*\.)*([a-zA-Z0-9\-])*([a-zA-Z0-9\/\-\.\?\=\%\&\,])*)')
+def matchurlPattern(url):
+    if urlPattern.fullmatch(url):
+        return True
+    else:
+        return False
+
+def querySite(url):
+    if matchurlPattern(url):
+        print("Correct URL Format: [ %s ]" % url)
+    else:
+        print("Wrong URL Format: [ %s ]" % url)
+
+querySite("www.google.com")
+querySite("https://www.google.com/")
