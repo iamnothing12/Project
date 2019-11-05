962b0840fb974b233f7d0143d8f05339aea78aba
iamnothing12 <derwin_lim@hotmail.com>
2019-11-05 22:27:10 +0800

    Update Path
    Creation of directory when scraping website.

---
+++ createPath.py
@@ -0,0 +1,41 @@
+import os
+
+def checkPath(path):
+    if not os.path.exists(str(path)):
+        return False
+    else:
+        return True
+def createPath(path):
+    if not checkPath(path):
+        try:
+            os.makedirs(str(path))
+        except OSError as e:
+            print("Error Creating ",str(path),": [ %s ]"% e)
+
+def deletePath(path):
+    if checkPath(path):
+        try:
+            os.rmdir(str(path))
+        except OSError as e:
+            print("Error Creating ",str(path),": [ %s ]"% e)
+
+def createFile(path,filename):
+    path = str(path)
+    if not checkPath(path):
+        createPath(path)
+    try:
+        with open(path, filename):
+            os.utime(path, None)
+    except OSError as e:
+        print("Error Creating ",str(path),": [ %s ]"% e)
+
+def listDirectory(path):
+    for root, dirs, files in os.walk(path):
+        for directory in dirs:
+            print(directory)
+
+# # Examples of Usage
+# createPath(12345)
+# listDirectory("/mnt/d/FYP")
+# deletePath(1234)
+# deletePath(12345)
