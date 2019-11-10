9e2c62d85eb27c9c7d6def03b52dda2f2d82d3fd
iamnothing12 <derwin_lim@hotmail.com>
2019-11-10 23:00:08 +0800

    Update Query

--- config.py
+++ config.py
@@ -188,7 +188,7 @@ ANDROID_CHROME = 'Mozilla/5.0 (Linux; Android 8.0.0;) AppleWebKit/537.36 (KHTML,
 ANDROID_FIREFOX = 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0'
 APPLE_CHROME = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/77.0.3865.103 Mobile/15E148 Safari/605.1'
 APPLE_SAFARI = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_0 like Mac OS X) AppleWebKit/602.1.38 (KHTML, like Gecko) Version/10.0 Mobile/14A41DS1497c Safari/602.1'
-WINDOWS_CHROME = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36'
+WINDOWS_CHROME = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'
 WINDOWS_EDGE = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; ServiceUI 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18362'
 
 
--- query.py
+++ query.py
@@ -18,7 +18,8 @@ spansrcList = []
 
 def init_PoolManager():
     urllib3.contrib.pyopenssl.inject_into_urllib3()
-    return urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
+    return urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+
 
 def check_link_contains(soup, tag):
     if '#' in str(soup.get(tag)) or \
@@ -27,7 +28,8 @@ def check_link_contains(soup, tag):
         return False
     return True
 
-def append_list(list,soup, tag):
+
+def append_list(list, soup, tag):
     if str(soup.get(tag)).startswith('//'):
         list.append('https:' + soup.get(tag))
     elif str(soup.get(tag)).startswith('/'):
@@ -35,77 +37,119 @@ def append_list(list,soup, tag):
     else:
         list.append(soup.get(tag))
 
+
 def find_div(soup):
     for div in soup.find_all(c.DIV):
         insert_a(div)
         insert_img(div)
         find_iframe(div)
 
+
 def find_script(soup):
     for script in soup.find_all(c.SCRIPT):
-        if check_link_contains(script,c.SRC):
+        if check_link_contains(script, c.SRC):
             if not script.get(c.SRC) in scriptsrcList:
-                append_list(scriptsrcList,script,c.SRC)
+                append_list(scriptsrcList, script, c.SRC)
+
+
 def find_iframe(soup):
     for iframe in soup.find_all(c.IFRAME):
-        if check_link_contains(iframe,c.SRC):
+        if check_link_contains(iframe, c.SRC):
             if not iframe.get(c.SRC) in iframesrcList:
-                append_list(iframesrcList, iframe,c.SRC)
+                append_list(iframesrcList, iframe, c.SRC)
+
 
 def find_span(soup):
     for span in soup.find_all(c.SPAN):
         if check_link_contains(span, c.SRC):
             if not span.get(c.SRC) in spansrcList:
-                append_list(spansrcList,span,c.SRC)
+                append_list(spansrcList, span, c.SRC)
+
 
 def insert_a(soup):
     for a in soup.find_all(c.HYPERLINK_A):
         if check_link_contains(a, c.HREF):
             if not (str(a.get(c.HREF)) in aHrefList):
-                append_list(aHrefList,a,c.HREF)
+                append_list(aHrefList, a, c.HREF)
+
 
 def insert_img(soup):
     for img in soup.find_all(c.IMG):
-       if check_link_contains(img,c.SRC):
-           if not (img.get(c.SRC) in imgsrcList):
-               append_list(imgsrcList,img,c.SRC)
-       if check_link_contains(img,c.SRCSET):
-           if not (img.get(c.SRCSET) in imgsrcsetList):
-               append_list(imgsrcsetList,img,c.SRCSET)
-
+        if check_link_contains(img, c.SRC):
+            if not (img.get(c.SRC) in imgsrcList):
+                append_list(imgsrcList, img, c.SRC)
+        if check_link_contains(img, c.SRCSET):
+            if not (img.get(c.SRCSET) in imgsrcsetList):
+                append_list(imgsrcsetList, img, c.SRCSET)
 
                 # print (script.get('src'))
 
+
 def queryPage(con, url):
     print(url)
-    page = con.request('GET',url,headers={'User-agent':c.WINDOWS_CHROME}, redirect=True)
+    page = con.request('GET', url, headers={'User-agent': c.WINDOWS_CHROME}, redirect=True)
     # opener = urllib3.request.build_opener()
     # f = opener.open(request)
     if page.status in c.GOOD_STATUS_LIST:
         print(page.status)
         soup = BeautifulSoup(page.data, features='lxml')
-        cp.createFile('miniclip', str(str(url).split("/")[2].split(".")[1]+".html"), str(soup.prettify))
+        cp.createFile(c.defaultPath, str(str(url).split("/")[2].split(".")[1] + ".html"), str(soup.prettify()))
         find_script(soup)
         find_div(soup)
         find_iframe(soup)
-        print("Div Href:")
+        find_span((soup))
+        insert_a(soup)
+        print("Div Href: %i" % len(aHrefList))
         for line in aHrefList:
             print(line)
-        #
-        print("IMG Src: ")
-        for line1 in imgsrcList:
-            print(line1)
-        #
-        print("IMG Srcset: ")
-        for line2 in imgsrcsetList:
-            print(line2)
-        # print(page.url)
-        print("Script: ")
-        for line3 in scriptsrcList:
-            print(line3)
-        print("IFRAME: ")
-        for line4 in iframesrcList:
-            print(line4)
+            pagelinks = con.request('GET', url, headers={'User-agent': c.WINDOWS_CHROME}, redirect=True)
+            if pagelinks.status in c.GOOD_STATUS_LIST:
+                extractlink = tldextract.extract(line)
+                print("Extract link: %s" % extractlink.registered_domain)
+                print("Suffix: %s" % str(extractlink.suffix))
+                if str(line).startswith("http://"):
+                    striplink = str(line).lstrip("http://")
+                    striplink = str(striplink).lstrip(str(extractlink.subdomain))
+                    striplink = str(striplink).lstrip(".")
+                    striplink = str(striplink).lstrip(str(extractlink.registered_domain))
+                    if striplink.startswith(extractlink.suffix):
+                        striplink = str(striplink).lstrip(".")
+                        striplink = str(striplink).lstrip(str(extractlink.suffix))
+                else:
+                    striplink = str(line).lstrip("https://")
+                    striplink = str(striplink).lstrip(str(extractlink.subdomain))
+                    striplink = str(striplink).lstrip(".")
+                    striplink = str(striplink).lstrip((str(extractlink.registered_domain)))
+                    if striplink.startswith(extractlink.suffix):
+                        striplink = str(striplink).lstrip(".")
+                        striplink = str(striplink).lstrip(str(extractlink.suffix))
+                    # if not striplink.endswith('/'):
+                    #     print ("Ends With [/]: %s" %striplink.rsplit('/'))
+                    # striplink = str(line).lstrip(str("https://" + str(extractlink.subdomain) + str(extractlink.registered_domain) + str(extractlink.suffix)))
+                print("Strip Link: %s" % striplink)
+                print("Default Path: %s" % c.defaultPath)
+                fullpath = str(c.defaultPath + striplink)
+                print("Full Path: %s"% fullpath)
+                # print("File name: %s" % str(str(str(line).split("/")[2].split(".")[1]+".html")))
+                souplink = BeautifulSoup(pagelinks.data, features='lxml')
+                cp.createFile(fullpath,str(str(line).split("/")[2].split(".")[1]+".html"), str(souplink.prettify()))
+        # #
+        # print("IMG Src: %i" % len(imgsrcList))
+        # for line1 in imgsrcList:
+        #     print(line1)
+        # # #
+        # print("IMG Srcset: %i" % len(imgsrcsetList))
+        # for line2 in imgsrcsetList:
+        #     print(line2)
+        # print("Script: %i" % len(scriptsrcList))
+        # for line3 in scriptsrcList:
+        #     print(line3)
+        # print("IFRAME: %i" % len(iframesrcList))
+        # for line4 in iframesrcList:
+        #     print(line4)
+        # print("Span: %i" % len(spansrcList))
+        # for line5 in spansrcList:
+        #     print(line5)
 
     elif page.status in c.REDIRECT_STATUS_LIST:
         print(page.status)
@@ -114,18 +158,19 @@ def queryPage(con, url):
         print(page.status)
         # print(page.url)
     elif page.status in c.SERVER_ERROR_STATUS_LIST:
-        print (page.status)
+        print(page.status)
+
 
 def extractDomain():
     ext = tldextract.extract(c.defaultURL)
     c.defaultPath = ext.domain
-    print ("Default Path: ",c.defaultPath)
+    print("Default Path: ", c.defaultPath)
     # print(c.defaultURL)
     # print("Something: ",ext.subdomain,ext.domain,ext.suffix)
     # url_parts = (u.subdomain, u.domain, u.suffix, rest)
 
-
     # print("Default Path:", ext.domain)
+
 
 con = init_PoolManager()
 # c.defaultpath = tldextract.extract('http://forums.bbc.co.uk/')
